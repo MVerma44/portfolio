738ff373837d551f0c991f267171c0369fe98942
MVerma44 <vmayank.921@gmail.com>
2022-07-30 14:51:22 +0530

    Portfolio section

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
 
+    return render_template('index.html')
 
 app.run(debug=True)
